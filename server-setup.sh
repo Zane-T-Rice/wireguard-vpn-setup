b033648e6d91019db1a9048f2010be6707ff1119
Zane Rice <zanetrice@gmail.com>
2024-05-16 19:02:28 -0400

    Futher support multiple clients
    The connection was terribly slow when sharing the same key pairs across multiple clients.
    So now this creates separate key pairs for each peer.

--- server-setup.sh
+++ server-setup.sh
@@ -5,7 +5,6 @@ PRIVATE_ADDRESS_OF_SERVER=10.20.10.1
 PRIVATE_ADDRESS_AND_PORTS_OF_LOCAL_CLIENTS=10.20.10.2,2456,2459-10.20.10.3
 WIREGUARD_PORT=51820
 WIREGUARD_CONFIG_FILE=/etc/wireguard/wg0.conf
-ALLOWED_CLIENT_IPS=10.20.10.2/32,10.20.10.3/32
 
 # Install Wireguard and Generate Keys
 sudo apt update
@@ -21,20 +20,22 @@ if sudo test ! -e "/etc/wireguard/server-private.key"; then
 fi
 
 # Client keys
-if sudo test ! -e "/etc/wireguard/client-private.key"; then
-  wg genkey | sudo tee /etc/wireguard/client-private.key
-  sudo chmod go= /etc/wireguard/client-private.key
-  sudo cat /etc/wireguard/client-private.key | wg pubkey | sudo tee /etc/wireguard/client-public.key
-fi
+CLIENT_IPS=$(echo "$PRIVATE_ADDRESS_AND_PORTS_OF_LOCAL_CLIENTS" | grep -o -e "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+")
+
+for client_ip in $(echo $CLIENT_IPS); do
+  if sudo test ! -e "/etc/wireguard/$client_ip-client-private.key"; then
+    wg genkey | sudo tee /etc/wireguard/$client_ip-client-private.key
+    sudo chmod go= /etc/wireguard/$client_ip-client-private.key
+    sudo cat /etc/wireguard/$client_ip-client-private.key | wg pubkey | sudo tee /etc/wireguard/$client_ip-client-public.key
+  fi
+done
 
 # Down WireGuard before changing the rules, because the down will fail later with the new rules.
 # Does nothing if WireGuard is not already running.
 sudo wg-quick down wg0
 sudo systemctl stop wg-quick@wg0.service
 
-# Store the things we need to build the config files
-SERVER_PRIVATE_KEY=$(sudo cat /etc/wireguard/server-private.key)
-CLIENT_PUBLIC_KEY=$(sudo cat /etc/wireguard/client-public.key)
+# Default Internet interface for routing rules.
 INTERNET_INTERFACE=$(ip route list default | cut -d " " -f 5)
 
 # Write the server's wireguard config file in $WIREGUARD_CONFIG_FILE
@@ -45,7 +46,7 @@ echo "[Interface]" | sudo tee $WIREGUARD_CONFIG_FILE
 echo "Address = $PRIVATE_ADDRESS_OF_SERVER/24" | sudo tee -a $WIREGUARD_CONFIG_FILE
 echo "MTU = 1420" | sudo tee -a $WIREGUARD_CONFIG_FILE
 echo "# remote server private key" | sudo tee -a $WIREGUARD_CONFIG_FILE
-echo "PrivateKey = $SERVER_PRIVATE_KEY" | sudo tee -a $WIREGUARD_CONFIG_FILE
+echo "PrivateKey = $(sudo cat /etc/wireguard/server-private.key)" | sudo tee -a $WIREGUARD_CONFIG_FILE
 echo "ListenPort = $WIREGUARD_PORT" | sudo tee -a $WIREGUARD_CONFIG_FILE
 echo "" | sudo tee -a $WIREGUARD_CONFIG_FILE
 echo "PostUp = ufw route allow in on wg0 out on $INTERNET_INTERFACE" | sudo tee -a $WIREGUARD_CONFIG_FILE
@@ -72,11 +73,15 @@ for client_with_ports in $(echo $CLIENTS_WITH_PORTS); do
     echo "PreDown = iptables -t nat -D PREROUTING -i $INTERNET_INTERFACE -p udp -m multiport --dports $PORTS -j DNAT --to-destination $CLIENT" | sudo tee -a $WIREGUARD_CONFIG_FILE
   fi
 done
-echo "" | sudo tee -a $WIREGUARD_CONFIG_FILE
-echo "[Peer]" | sudo tee -a $WIREGUARD_CONFIG_FILE
-echo "AllowedIPs = $ALLOWED_CLIENT_IPS" | sudo tee -a $WIREGUARD_CONFIG_FILE
-echo "# local client public key" | sudo tee -a $WIREGUARD_CONFIG_FILE
-echo "PublicKey = $CLIENT_PUBLIC_KEY" | sudo tee -a $WIREGUARD_CONFIG_FILE
+
+# Set up peers
+for client_ip in $(echo $CLIENT_IPS); do
+  echo "" | sudo tee -a $WIREGUARD_CONFIG_FILE
+  echo "[Peer]" | sudo tee -a $WIREGUARD_CONFIG_FILE
+  echo "AllowedIPs = $client_ip/32" | sudo tee -a $WIREGUARD_CONFIG_FILE
+  echo "# local client public key" | sudo tee -a $WIREGUARD_CONFIG_FILE
+  echo "PublicKey = $(sudo cat /etc/wireguard/$client_ip-client-public.key)" | sudo tee -a $WIREGUARD_CONFIG_FILE
+done
 
 # Make a reasonable attempt to enable ipv4 packet forwarding.
 sudo sed -E "s/#? ?net.ipv4.ip_forward=[01]?/net.ipv4.ip_forward=1/" -i /etc/sysctl.conf
